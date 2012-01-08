d06b8dce95a0983d66d168175dcc2f593cbaaeb4
Leon Handreke <leonh@ndreke.de>
2012-01-08 16:29:29 +0100

    Print a nice invoice

--- turboolaf.py
+++ turboolaf.py
@@ -2,6 +2,8 @@
 
 import json
 
+INVOICE_WIDTH = 40
+
 print("Welcome to TurboOlaf!")
 
 invoice = json.load(open("invoice.json"))
@@ -22,5 +24,26 @@ while True:
         except:
             print("No such product!")
 
-print(invoice)
-print("bye!")
+print("\n\n")
+print("*" * INVOICE_WIDTH)
+print("K1 Getränkeverkauf".center(INVOICE_WIDTH))
+print("*" * INVOICE_WIDTH + "\n")
+
+total_price = 0
+for code, product in invoice.items():
+    if product["quantity"] > 0:
+        billed_price = product["price"] * product["quantity"]
+        total_price += billed_price
+
+        print(
+                product["name"].ljust(15) +
+                str(product["price"]).rjust(5) +
+                str(" x " + str(product["quantity"])).rjust(6) +
+                " = " +
+                str(billed_price).rjust(6)
+                )
+
+print("-" * INVOICE_WIDTH)
+print("Total:" + str(total_price).rjust(INVOICE_WIDTH - 6))
+print("\n")
+
